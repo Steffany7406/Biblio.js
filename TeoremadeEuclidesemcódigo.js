080eb5edfb7548b36a8881bf73fb3e5f66adc491
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-10-29 16:47:01 -0300

    Update TeoremadeEuclidesemcódigo.js

--- TeoremadeEuclidesemcódigo.js
+++ TeoremadeEuclidesemcódigo.js
@@ -1,13 +1,13 @@
 var a = 9
 var b = 27
 
-function gcd(a, b) {
+function gcd(a, b) { //  function to calculate GCD
     var R;
-    while ((a % b) > 0)  {
+    while ((a % b) > 0)  {  // while remainder is not zero
       R = a % b;
       a = b;
       b = R;
     }
-    return b;
+    return b; 
   }
-  
+  // Para rodar o  código, você pode usar o console.log(gcd(a, b)) ou console.log(gcd(9, 27))
